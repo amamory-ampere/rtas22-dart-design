c47faf399fa1f7e5266d3cfc4b55665111337134
Alexandre Amory <alexandre.amory@santannapisa.it>
2022-09-30 13:33:49 +0000

    cleaner lenet power csv; better run.sh script

--- lenet/sw/run.sh
+++ lenet/sw/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+# the old power sampler from
 #~/repos/power-sampler/build/rt-dag-sampler  > power.csv 2>&1 &
-~/repos/rtas22-dart-design/power-sampler/power-sampler -v -o power.csv &
+# the new power sampler. use -v to get the values for all power rails
+#../../power-sampler/power-sampler -v -o temp_power.csv &
+../../power-sampler/power-sampler -o temp_power.csv &
 
 PID=$!
 echo "power sample PID: $PID"
@@ -15,4 +19,8 @@ app_pid+=($!)
 wait "${app_pid[@]}"
 sleep 0.1
 kill $PID
+# usually the last line is not complete due to the kill. 
+# thus, this command discards the last line
+head -n -1 temp_power.csv > power.csv
+
 
