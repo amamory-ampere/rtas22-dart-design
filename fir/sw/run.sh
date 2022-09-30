782fa50bd1d22fb3470302676ba995e9e5991e4b
Alexandre Amory <alexandre.amory@santannapisa.it>
2022-09-30 16:19:15 +0000

    update fir power

--- fir/sw/run.sh
+++ fir/sw/run.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-~/repos/power-sampler/build/rt-dag-sampler  > power.csv 2>&1 &
-PID=$!
-echo "power sample PID: $PID"
-sleep 0.1
+
+# the old power sampler from
+#~/repos/power-sampler/build/rt-dag-sampler  > power.csv 2>&1 &
+# the new power sampler. use -v to get the values for all power rails
+#../../power-sampler/power-sampler -v -o temp_power.csv &
+../../power-sampler/power-sampler -o temp_power.csv &
+sleep 1
 
 # launch the apps
 app_pid=()
@@ -11,6 +14,10 @@ app_pid+=($!)
 
 # wait the apps to finish and kill the power sampler
 wait "${app_pid[@]}"
-sleep 0.1
-kill $PID
+sleep 3
+killall power-sampler
+
+# usually the last line is not complete due to the kill. 
+# thus, this command discards the last line
+head -n -1 temp_power.csv > power.csv
 
