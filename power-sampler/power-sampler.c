8bc7c3638d31a4e91d0943be08928ad10d001a29
Alexandre Amory <alexandre.amory@santannapisa.it>
2022-10-03 10:07:52 +0200

    new version of power sampler which buffer data into memory to reduce the writes to disk

--- power-sampler/power-sampler.c
+++ power-sampler/power-sampler.c
@@ -1,6 +1,11 @@
 /*
-source code from:
+original source code from:
 https://www.xilinx.com/developer/articles/accurate-design-power-measurement.html
+including several modifications, such as:
+ - change the default values for sampling frequency and integrations
+ - reduce printing and saving to increase the sampling frequency. data is saved into memory
+ - power calculation changed to: abs(in1-in2) * curr1
+ - output format change to CSV be used w gnuplot
 
 compile w:
 $ gcc power-sampler.c -o power-sampler
@@ -16,6 +21,7 @@ improve the sampling frequency by buffering all data into memory and write it in
 #include <stdio.h>
 #include <unistd.h>
 #include <signal.h>
+#include <assert.h>
 
 //These are specific to ZCU102
 #define VCCPSINTFP 0
@@ -59,9 +65,7 @@ const char railname_arr[50][12] = {
 };
 
 
-
 typedef struct ina {
-
 	char current_path[50];
 	char voltage1_path[50];
     char voltage2_path[50];
@@ -69,8 +73,15 @@ typedef struct ina {
 	int current;
 	int voltage;
 	int last;
-
 } ina;
+
+// global buffer to save all collected data in memory to increase the samplig frequency
+int g_n_sensors = 0;
+int g_n_samples = 0;
+int * g_buffer=NULL;
+#define MAX_BUFFER_LINES 10000
+char target_file[50];
+ina inas[30];
 
 int cmp_ina(const void *a, const void *b) {
 	ina *temp1 = (ina*)a;
@@ -88,7 +99,7 @@ int cmp_ina(const void *a, const void *b) {
 
 }
 
-void populate_ina_array(ina *inas) {
+void populate_ina_array() {
 	DIR *d;
 	struct dirent *dir;
 
@@ -133,6 +144,7 @@ void populate_ina_array(ina *inas) {
 		}
 
 	}
+    g_n_sensors = counter;
 
 	qsort(inas, counter, sizeof(ina), cmp_ina);
 	if (counter > 0)
@@ -146,12 +158,11 @@ void populate_ina_array(ina *inas) {
 
 		counter++;
 	}
-
 	closedir(d);
 
 }
 
-void list_inas (ina *inas) {
+void list_inas () {
 	int counter = 0;
 	while(1) {
 		printf("Found INA%03d at dir: %s\n", counter, inas[counter].current_path);
@@ -163,35 +174,15 @@ void list_inas (ina *inas) {
 	return;
 }
 
-void run_bm (char target_file[50], int sleep_per, unsigned iterations, int verbose, ina *inas) {
-	FILE *sav_ptr;
+void run_bm (int sleep_per, unsigned iterations, int verbose) {
 	FILE *ina_ptr;
-
-	sav_ptr = fopen(target_file, "w");
-
 	char buffer[20];
-    int v1,v2;
-	float plpower = 0;
-	float pspower = 0;
-	float mgtpower = 0;
-
-	int counter = 0;
-	while(1) {
-		if (verbose == 1) {
-			fprintf(sav_ptr, "\"%s (uV)\",\"%s (uA)\",", inas[counter].name, inas[counter].name);
-		}
-		if(inas[counter].last == 1)
-			break;
-
-		counter++;
-	}
-    // print the last columns
-    fprintf(sav_ptr, "\"PS Power(W)\",\"PL Power(W)\",\"MGT Power(W)\",\"Total Power(W)\"\n");
+    int v1,v2,current;
+	int counter;
 
 	for (unsigned j = 0; j < iterations; j++) {
 		counter = 0;
 		while(1) {
-
 			ina_ptr = fopen(inas[counter].voltage1_path, "r");
 			fscanf(ina_ptr,"%[^\n]", buffer);
 			v1 = atoi(buffer);
@@ -201,73 +192,101 @@ void run_bm (char target_file[50], int sleep_per, unsigned iterations, int verbo
 			fscanf(ina_ptr,"%[^\n]", buffer);
 			v2 = atoi(buffer);
 			fclose(ina_ptr);
-
             inas[counter].voltage = labs(v1-v2);
 
-			if(verbose==1) {
-				//printf("Voltage # %d = %d \n", counter, atoi(buffer));
-				fprintf(sav_ptr, "%d,", inas[counter].voltage);
-			}
-
 			ina_ptr = fopen(inas[counter].current_path, "r");
-
 			fscanf(ina_ptr,"%[^\n]", buffer);
+			current = atoi(buffer);
+            fclose(ina_ptr);
 
-			inas[counter].current = atoi(buffer);
-			if(verbose==1) {
-				//printf("Current # %d = %d \n", counter, atoi(buffer));
-				fprintf(sav_ptr, "%s,", buffer);
-			}
+            assert(iterations<=MAX_BUFFER_LINES);
+            g_buffer[iterations*g_n_sensors + counter] = labs(v1-v2) * current;
 
 			if(inas[counter].last) {
-
-				pspower = (float) (inas[VCCPSINTFP].voltage*inas[VCCPSINTFP].current+
-						inas[VCCINTLP].voltage*inas[VCCINTLP].current+
-						inas[VCCPSAUX].voltage*inas[VCCPSAUX].current+
-						inas[VCCPSPLL].voltage*inas[VCCPSPLL].current+
-						inas[VCCPSDDR].voltage*inas[VCCPSDDR].current+
-						//inas[VCCOPS].voltage*inas[VCCOPS].current+
-						//inas[VCCOPS3].voltage*inas[VCCOPS3].current+
-						inas[VCCPSDDRPLL].voltage*inas[VCCPSDDRPLL].current)/1000000.0;
-
-				plpower = (float) (inas[VCCINT].voltage*inas[VCCINT].current+
-						inas[VCCBRAM].voltage*inas[VCCBRAM].current+
-						inas[VCCAUX].voltage*inas[VCCAUX].current+
-						inas[VCC1V2].voltage*inas[VCC1V2].current+
-						inas[VCC3V3].voltage*inas[VCC3V3].current)/1000000.0;
-
-				mgtpower = (float) (inas[MGTRAVCC].voltage*inas[MGTRAVCC].current+
-						inas[MGTRAVTT].voltage*inas[MGTRAVTT].current+
-						inas[MGTAVCC].voltage*inas[MGTAVCC].current+
-						inas[MGTAVTT].voltage*inas[MGTAVTT].current+
-						inas[VCC3V3].voltage*inas[VCC3V3].current)/1000000.0;
-
-				fprintf(sav_ptr, "%g,%g,%g,%g\n", pspower, plpower, mgtpower, mgtpower+plpower+pspower);
-				fclose(ina_ptr);
 				break;
 			}
-			fclose(ina_ptr);
 			counter++;
 		}
-
+        ++g_n_samples;
     	sleep(sleep_per);
 	}
-	fclose(sav_ptr);
 }
 
 void exit_all(int sigid){
     printf("Exting\n");
+    unsigned line;
+    float pspower, plpower, mgtpower;
+	FILE *sav_ptr;
+	sav_ptr = fopen(target_file, "w");
+
+    // save the CSV header
+    int counter=0;
+	while(1) {
+    	fprintf(sav_ptr, "\"%s (uW)\",", inas[counter].name);
+		if(inas[counter].last == 1)
+			break;
+		counter++;
+	}
+    // print the last columns
+    fprintf(sav_ptr, "\"PS Power(W)\",\"PL Power(W)\",\"MGT Power(W)\",\"Total Power(W)\"\n");
+
+	for (unsigned j = 0; j < g_n_samples; j++) {
+        line = j*g_n_sensors;
+
+        // print the sensor data
+        counter=0;
+		while(1) {
+            fprintf(sav_ptr, "%d,", g_buffer[line + counter]);
+			if(inas[counter].last) {
+				break;
+			}
+			counter++;
+		}
+
+        // print the totals
+        pspower = (float) (
+            g_buffer[line + VCCPSINTFP]+
+            g_buffer[line + VCCINTLP]+
+            g_buffer[line + VCCPSAUX]+
+            g_buffer[line + VCCPSPLL]+
+            g_buffer[line + VCCPSDDR]+
+            //inas[VCCOPS].voltage*inas[VCCOPS].current+
+            //inas[VCCOPS3].voltage*inas[VCCOPS3].current+
+            g_buffer[line + VCCPSDDRPLL])/1000000.0;
+
+        plpower = (float) (
+                g_buffer[line + VCCINT]+
+                g_buffer[line + VCCBRAM]+
+                g_buffer[line + VCCAUX]+
+                g_buffer[line + VCC1V2]+
+                g_buffer[line + VCC3V3])/1000000.0;
+
+        mgtpower = (float) (
+                g_buffer[line + MGTRAVCC]+
+                g_buffer[line + MGTRAVTT]+
+                g_buffer[line + MGTAVCC]+
+                g_buffer[line + MGTAVTT]+
+                g_buffer[line + VCC3V3])/1000000.0;
+
+        fprintf(sav_ptr, "%g,%g,%g,%g\n", pspower, plpower, mgtpower, mgtpower+plpower+pspower);
+    }
+
+    fclose(sav_ptr);
     exit(0);
 }
 
 int main(int argc, char *argv[]) {
 
-	ina inas[30];
 	populate_ina_array(inas);
 
+    // the normal usecase for this app is to be interrupted by CTRL+C or kill, 
+    // so this will dump all buffered data into file after the app is actually closed
     signal(SIGKILL,exit_all);
     signal(SIGINT,exit_all);
     signal(SIGTERM,exit_all);
+
+    // the global buffer is
+    g_buffer = (int*) malloc(sizeof(int)*g_n_sensors*MAX_BUFFER_LINES);
 
 	int opt;
     // run as fast as possible
@@ -275,7 +294,7 @@ int main(int argc, char *argv[]) {
     	// run until the end of times ... or a CTRL+C
 	unsigned iterations = UINT32_MAX;
 	int verbose = 0;
-	char target_file[50] = "./out.txt";
+    strcpy(target_file, "./out.txt");
 
 	while ((opt = getopt(argc, argv, "t:o:vn:l")) != -1) {
 
@@ -302,7 +321,7 @@ int main(int argc, char *argv[]) {
 				break;
 		}
 	}
-	run_bm(target_file, sleep_per, iterations, verbose, inas);
+	run_bm(sleep_per, iterations, verbose);
 
 	return 0;
 }
