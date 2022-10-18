626c128a4fbb3690da75d7f6ca1668d341bac8eb
Alexandre Amory <alexandre.amory@santannapisa.it>
2022-10-18 08:06:07 +0000

    updating the script to generate the power profiling. also using rt-dag to get power prof

--- power_prof/cpu-power-profiling-rt-dag.sh
+++ power_prof/cpu-power-profiling-rt-dag.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-set -e
 
 cpufreq-set -g userspace
 taskset -c 1 ../power-sampler/power-sampler -o temp_power.csv &
-#../power-sampler/power-sampler -o temp_power.csv &
 
 sleep 2
 # the idle power for each OPP
@@ -21,17 +19,13 @@ cpufreq-set -f 1199999
 sleep 10
 
 # launch the apps to create the scenario for busy power
+#export TICKS_PER_US=3
 echo "BUSY at 300MHz"
 cpufreq-set -f 299999 
-# place each workload to a different core and at the highest priority, dont use chrt 99 which will cause errors in zcu102
-#taskset -c 0 nice -n -20 yes > /dev/null &
-#taskset -c 1 nice -n -20 yes > /dev/null &
-#taskset -c 2 nice -n -20 yes > /dev/null &
-taskset -c 3 nice -n -20 yes > /dev/null &
-#yes > /dev/null &
-#yes > /dev/null &
-#yes > /dev/null &
-#yes > /dev/null &
+# uncomment this to run a specific rt-dag scenario
+#~/repos/power-aware-optimization/tools/rt-dag/rt-dag/build/rt_dag ~/repos/power-aware-optimization/tools/rt-dag/rt-dag/cpu-overload.yaml
+# this is the workload executed in a loop by rt-dag
+taskset -c 3 nice -n -20 ~/repos/power-aware-optimization/tools/rt-dag/rt-dag/build/rt_dag -t 10000000000 &
 sleep 10
 echo "BUSY at 400MHz"
 cpufreq-set -f 399999
@@ -43,11 +37,11 @@ echo "BUSY at 1200MHz"
 cpufreq-set -f 1199999
 sleep 10
 
-killall yes
+killall rt_dag
 sleep 2
 killall power-sampler
 # usually the last line is not complete due to the kill. 
 # thus, this command discards the last line
 head -n -1 temp_power.csv > temp_power2.csv
 # prepare data for gnuplot
-cut -d',' -f37- temp_power2.csv > power.gplot
+cut -d',' -f37- temp_power2.csv > power-rt-dag.gplot
